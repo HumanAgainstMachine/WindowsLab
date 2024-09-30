bcda15090de5977d191f9382ba71b4a7a394ba92
HumanAgainstMachine <ezio.tarquilio@ikmail.com>
2024-09-30 07:45:33 +0200

    Unimportant changes

--- WindowsLab/WinLab-Debug.ps1
+++ WindowsLab/WinLab-Debug.ps1
@@ -1,34 +1,9 @@
-# Remove stop time trigger
 
-$DailyTime = '23:00'
-$dailyTimeObj = [DateTime]::ParseExact($DailyTime, "HH:mm", [System.Globalization.CultureInfo]::InvariantCulture)
+# Read the content of a binary file into a byte array
+$filePath = "C:\Users\Admin\Desktop\CV.odt"
+$fileContent = [System.IO.File]::ReadAllBytes($filePath)
 
-$dailyStopTime = $dailyTimeObj.TimeOfDay 
+# Now $fileContent holds the binary content of the file
 
-try {
-    # Get scheduled StopThisComputer task if exist
-    $stopThisComputerTask = Get-ScheduledTask -TaskName:'StopThisComputer' -TaskPath:'\WinLabAdmin\' -ErrorAction Stop
-}
-catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException] {
-    # $_.exception.GetType().fullname
-    Write-Host "StopThisComputer task not exist, nothing to remove so"
-    Return $null
-}            
+[System.IO.File]::WriteAllBytes("C:\Users\Admin\Desktop\mamt.odt", $filecontent) 
 
-# Set principal contex for SYSTEM account to run as a service with with the highest privileges
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest            
-
-# Remove the given time stop trigger 
-$triggers = @()
-foreach ($trg in $stopThisComputerTask.Triggers) {
-    if (([datetime] $trg.StartBoundary).TimeOfDay -ne $dailyStopTime) {
-        $triggers += $trg
-    }
-}
-
-if ($triggers.count -lt $stopThisComputerTask.Triggers.count) {
-    Set-ScheduledTask -TaskName:'StopThisComputer' -TaskPath:'\WinLabAdmin\' -Trigger $triggers -Principal $principal | Out-Null
-    Write-Host "Stop daily time $DailyTime removed on $env:computername" -ForegroundColor Green    
-} else {
-    Write-Host "Stop daily time $DailyTime not exist on $env:computername" -ForegroundColor Red
-}
